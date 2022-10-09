0e86cdc4f7d619b2ce99a968ab95293c77681564
MoreiraSte <casagrandestephany91@gmail.com>
2022-10-09 12:49:58 -0300

    new container events

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,18 +24,18 @@ function auto(){
 window.onload = auto;
 
 /*===================event========================*/
-// function myFunction(imgs) {
-//     // Get the expanded image
-//     var expandImg = document.getElementById("expandedImg");
-//     // Get the image text
-//     var imgText = document.getElementById("imgtext");
-//     // Use the same src in the expanded image as the image being clicked on from the grid
-//     expandImg.src = imgs.src;
-//     // Use the value of the alt attribute of the clickable image as text inside the expanded image
-//     imgText.innerHTML = imgs.alt;
-//     // Show the container element (hidden with CSS)
-//     expandImg.parentElement.style.display = "block";
-//   }
+function myFunction(imgs) {
+    // Get the expanded image
+    var expandImg = document.getElementById("expandedImg");
+    // Get the image text
+    var imgText = document.getElementById("imgtext");
+    // Use the same src in the expanded image as the image being clicked on from the grid
+    expandImg.src = imgs.src;
+    // Use the value of the alt attribute of the clickable image as text inside the expanded image
+    imgText.innerHTML = imgs.alt;
+    // Show the container element (hidden with CSS)
+    expandImg.parentElement.style.display = "block";
+  }
 /*==================== SHOW MENU ====================*/
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
